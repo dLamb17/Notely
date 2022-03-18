89eb63ad4dc8bc3bea4cacae675110f167458d37
desmo <desmo@LAPTOP-FR73ID9H>
2022-03-17 22:17:48 -0400

    Add project files.

---
+++ Notely Application/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Notely_Application.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
